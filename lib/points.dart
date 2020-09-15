44bccd7e2a0197bc6564b86b2c6655404dd6c9e2
SurfaceW <surfacew@163.com>
2020-09-15 13:32:38 +0800

    feat: 🎸 add type system

--- lib/points.dart
+++ lib/points.dart
@@ -5,11 +5,13 @@ library points;
 
 import 'dart:math';
 
+import 'dart:svg';
+
 export 'extra.dart';
 
 part './constants.dart';
 
-final _pointsCollection = [];
+final List<Point> pointList = [];
 
 // work for interface
 abstract class APoint {
@@ -20,17 +22,17 @@ abstract class APoint {
 class Point implements APoint {
   var x, y;
   final z = 1;
-  get xPlus1 => x + 1;
-  set xPlus1(y) {
+  int get xPlus1 => x + 1;
+  void set xPlus1(int y) {
     x = y + 1;
   }
 
   Point(this.x, this.y);
-  scale(factor) => new Point(x * factor, y * factor);
+  Point scale(factor) => new Point(x * factor, y * factor);
 
   // refine the operator
-  operator +(p) => new Point(x + p.x, y + p.y);
-  operator ==(p) => p.x == x && p.y == y;
+  Point operator +(p) => new Point(x + p.x, y + p.y);
+  bool operator ==(p) => p.x == x && p.y == y;
 
   static var PI = 3.141592653;
   static distance(p1, p2) {
@@ -47,19 +49,25 @@ class Point3D extends Point {
   // Point3D initializer => Point initializer => Object initializer
   // => Object constructor => Point constrcutor => Point3D constrcutor
   Point3D(a, b, c) : super(a, b) {
-    z = c;
+    // type reification
+    if (!(c is int)) {
+      throw 'Type Error, third parameter should be a int type';
+    }
+    // type cast
+    z = c as int;
   }
 
   @override
   noSuchMethod(Invocation invocation) {
     // you can inject factory method here
+
     return super.noSuchMethod(invocation);
   }
 }
 
 class ZInitializer {
   var z;
-  fetch() {
+  void fetch() {
     z = 4;
   }
 }
